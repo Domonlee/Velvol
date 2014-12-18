343dd3e5a5fded7f51b0f4d005202b3f5e7cb079
Qiu800820 <qiujunsen@163.com>
2014-12-18 10:03:09 +0800

    v0.015
    delete menu

--- src/com/velvol/o2o/CateWelActivity.java
+++ src/com/velvol/o2o/CateWelActivity.java
@@ -23,10 +23,4 @@ public class CateWelActivity extends Activity {
 					}
 				});
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.cate_wel, menu);
-		return true;
-	}
 }
