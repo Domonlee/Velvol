81c88ee53cfac127fd9f069c0970f4c88619bb30
Qiu800820 <qiujunsen@163.com>
2014-12-31 17:18:05 +0800

    v0.033-qiu
    整合版本，添加百度地图

---
+++ src/com/velvol/o2o/ui/make/DialogMap.java
@@ -0,0 +1,67 @@
+package com.velvol.o2o.ui.make;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+import com.velvol.o2o.R;
+
+public class DialogMap {
+	public Dialog dialog;
+
+	public View showDitu(Activity activity, double jingdu, double weidu) {
+		if(dialog != null)
+			dialog.cancel();
+		View layout = LayoutInflater.from(activity).inflate(
+				R.layout.dialog_shopdetails, null);
+		MapView mMapView = (MapView) layout.findViewById(R.id.bmapView);
+		ImageView dingwei = (ImageView) layout.findViewById(R.id.iv_map);
+		
+		final BaiduMap mBaiduMap = mMapView.getMap();
+		
+		// 开启交通图
+		mBaiduMap.setTrafficEnabled(false);
+		// 定义Maker坐标点
+		LatLng point = new LatLng(weidu, jingdu);
+		// 构建Marker图标
+		BitmapDescriptor bitmap = BitmapDescriptorFactory
+				.fromResource(R.drawable.address);
+		// 构建MarkerOption，用于在地图上添加Marker
+		final OverlayOptions option = new MarkerOptions().position(point).icon(
+				bitmap);
+		// 在地图上添加Marker，并显示
+		mBaiduMap.addOverlay(option);
+		
+		// 设置比例尺 15表示比例尺级别
+		final MapStatusUpdate msu = MapStatusUpdateFactory.newLatLngZoom(point,
+				15);
+		mBaiduMap.setMapStatus(msu);
+		dingwei.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// 重新定位
+				mBaiduMap.setMapStatus(msu);
+			}
+		});
+
+		dialog = new Dialog(activity, R.style.dialog);
+		dialog.setContentView(layout);
+		dialog.setCanceledOnTouchOutside(true);
+		dialog.show();
+		return layout;
+	}
+
+}
--- src/com/velvol/o2o/ui/make/ShopDetailsActivity.java
+++ src/com/velvol/o2o/ui/make/ShopDetailsActivity.java
@@ -1,7 +1,11 @@
 package com.velvol.o2o.ui.make;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.baidu.mapapi.SDKInitializer;
 import com.velvol.o2o.R;
 import com.velvol.o2o.adapter.make.ShopDetailsAdapter;
 import com.velvol.o2o.tool.BaseActivity;
@@ -9,13 +13,17 @@ import com.velvol.o2o.tool.MyListView;
 
 public class ShopDetailsActivity extends BaseActivity {
 	
+	private TextView show_map;
+	private ImageView back;
 	private MyListView listView;
 	private ShopDetailsAdapter adapter;
+	private DialogMap map; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		this.requestWindowFeature(1);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_make_shopdetails);
+		SDKInitializer.initialize(getApplicationContext());
 		findViewById();
 		initView();
 	}
@@ -23,14 +31,32 @@ public class ShopDetailsActivity extends BaseActivity {
 	@Override
 	protected void findViewById() {
 		listView = (MyListView)findViewById(R.id.listview);
+		show_map = (TextView)findViewById(R.id.show_map);
+		back = (ImageView)findViewById(R.id.title_topbar_back_iv);
 	}
 
 	@Override
 	protected void initView() {
 		adapter = new ShopDetailsAdapter(ShopDetailsActivity.this);
 		listView.setAdapter(adapter);
+		show_map.setOnClickListener(listener);
+		back.setOnClickListener(listener);
+		map = new DialogMap();
 	}
-
+	View.OnClickListener listener = new View.OnClickListener() {
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.show_map:
+				map.showDitu(ShopDetailsActivity.this, 34.260921*1E6, 108.895761*1E6);
+				break;
+			case R.id.title_topbar_back_iv:
+				finish();
+				break;
+			default:
+				break;
+			}
+		}
+	};
 	@Override
 	protected void result(String result) {
 
